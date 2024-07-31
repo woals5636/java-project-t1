18381a54d0c8d620064382186670f3ee9c3a52d4
Deneb0000 <119793723+Deneb0000@users.noreply.github.com>
2024-07-31 17:04:58 +0900

    Update SaveFile.java
     String info = String.format("%s - %d : %d : %d",name,scoreBoardSet,scoreBoardGame,scoreBoardScore);

--- JavaProjectT1/src/tennisGame/SaveFile.java
+++ JavaProjectT1/src/tennisGame/SaveFile.java
@@ -14,7 +14,7 @@ public class SaveFile {
     public void saveScorePalyer(String name, int scoreBoardSet/*임의의세트*/, int scoreBoardGame/*임의의게임*/, int scoreBoardScore/*임의의점수*/) {
         try (FileWriter writer = new FileWriter(fileName)) {
             // 요소와 점수를 파일에 저장
-            String info = String.format("%s",name);
+             String info = String.format("%s - %d : %d : %d",name,scoreBoardSet,scoreBoardGame,scoreBoardScore);
             writer.append(info);
             writer.flush();
             System.out.println(info);
