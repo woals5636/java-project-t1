55e1c63109ebf3a06207baf32149693b0dd09660
seho <tlswjseks@gmail.com>
2024-07-31 15:14:41 +0900

    modify Player

--- JavaProjectT1/src/tennisGame/Main.java
+++ JavaProjectT1/src/tennisGame/Main.java
@@ -1,15 +1,30 @@
 package tennisGame;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		// 3세트 or 5세트
 		// 플레이어 2명
 		// 이름을 입력 받을 것이고
 		// 그러려면 플레이어를 생성하는 객체를 만들어야지
-		
+
 		Player p1 = new Player();
 		p1.GameStart();
+
+		// 게임 시작
+		// 점수가 랜덤으로 오르는 테니스 점수판 출력되게
+
+		ScoreBoard sb = new ScoreBoard();
+
+
+
+		// 게임 종료 안내
+		System.out.println("경기가 종료되었습니다.");
+
 	}
 }
--- JavaProjectT1/src/tennisGame/Player.java
+++ JavaProjectT1/src/tennisGame/Player.java
@@ -1,51 +1,44 @@
 package tennisGame;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Player {
-	
-	String [] name = new String [2];
-	int setNumber = 0;
 
-	Scanner scanner = new Scanner(System.in);
+    private String[] name = new String[2];
+    private int setNumber = 0;
+    private Scanner scanner = new Scanner(System.in);
 
-	public void GameStart() {
-		//3세트 or 5세트
-		System.out.println("테니스 세트를 선택합니다.");
-		System.out.println("3세트 : (1)입력, 5세트 : (2)입력");
-		int setNumber = scanner.nextInt();
+    public void GameStart() {
+        // 3세트 또는 5세트 선택
+        System.out.println("테니스 세트를 선택합니다.");
+        System.out.println("3세트 : (1)입력, 5세트 : (2)입력");
+        
+        // 입력 검증 루프
+        while (true) {
+            setNumber = scanner.nextInt();
+            if (setNumber == 1 || setNumber == 2) {
+                break;
+            }
+            System.out.println("잘못된 입력입니다.");
+            System.out.println("다시 입력 - 3세트 : (1)입력, 5세트 : (2)입력");
+        }
 
-		// 세트 확인 출력
-		if(setNumber==1)System.out.println("3세트 선택");
-		else if(setNumber==2)System.out.println("5세트 선택");
-		
-		while (!(setNumber==1 || setNumber == 2)) {
-			System.out.println("잘못된 입력입니다.");
-			System.out.println("다시 입력 - 3세트 : (1)입력, 5세트 : (2)입력");
-			setNumber = scanner.nextInt();
-			
-			
-			
-			try {
-				System.in.skip(System.in.available());          //버퍼 비우기
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	
-			
-		}//while
-		
-		
-		// 선수 이름 입력
-		String [] name = new String [2];
-		for (int i = 0; i < name.length; i++) {
-			System.out.printf("%d번 선수의 이름을 입력하세요 :  ", i+1);
-			name[i] = scanner.next();
-		}//for
-		
+        // 세트 확인 출력
+        if (setNumber == 1) {
+            System.out.println("3세트 선택");
+        } else {
+            System.out.println("5세트 선택");
+        }
 
-	}//Gamestart()
+        // 선수 이름 입력
+        for (int i = 0; i < name.length; i++) {
+            System.out.printf("%d번 선수의 이름을 입력하세요: ", i + 1);
+            name[i] = scanner.next();
+        }
+
+        // Scanner 닫기
+        scanner.close();
+    }
 
 	
 	
