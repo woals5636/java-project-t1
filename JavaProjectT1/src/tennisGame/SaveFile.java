b1b54b04350b52c1380feac119c493d8ccf46a4c
Deneb0000 <119793723+Deneb0000@users.noreply.github.com>
2024-07-31 13:58:53 +0900

    Update SaveFile.java
    코딩 다하고 정리하면서 출력하는게 좋다고 하셨는데..
    일단은 gpt한테 물어보니깐 이런식으로 짜줘서 읽어보면서 수정하고 있습니다

--- JavaProjectT1/src/tennisGame/SaveFile.java
+++ JavaProjectT1/src/tennisGame/SaveFile.java
@@ -1,5 +1,26 @@
 package tennisGame;
 
 public class SaveFile {
+   private String fileName;
 
+    public SaveFile(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void saveScorePalyer(String palyer, int scoreBoard) {
+        try (FileWriter writer = new FileWriter(fileName, true)) {
+            // 요소와 점수를 파일에 저장
+            writer.write(palyer + "," + scoreBoard + "\n");
+//            System.out.println("요소 '" + palyer + "'와 점수 " + score + "를 파일에 저장했습니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // 사용 예시
+        SaveFile saveFile = new SaveFile("palyers_scores.txt");
+        saveFile.saveScorePalyer("Hydrogen", 1.008);
+        saveFile.saveScorePalyer("Oxygen", 16.00);
+    }
 }
